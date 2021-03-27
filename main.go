ce87608e5959c3a3b74c381d2436d128ff7be231
Alexander <alexbidenko1998@gmail.com>
2021-03-27 21:08:55 +0300

    Test cors

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"rguide/dif"
 )
 
+var headers = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header"
+
 func handler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "Hello RGuide!")
 }
@@ -24,6 +26,14 @@ func main() {
 
 	r := mux.NewRouter()
 
+	r.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("OPTIONS")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", headers)
+		w.WriteHeader(http.StatusNoContent)
+		return
+	})
 	r.Use(accessControlMiddleware)
 
 	r.PathPrefix("/files/previews/").Handler(http.StripPrefix("/files/previews/", http.FileServer(http.Dir("/var/www/files-preview"))))
@@ -39,11 +49,8 @@ func accessControlMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
-
-		if r.Method == "OPTIONS" {
-			return
-		}
+		w.Header().Set("Access-Control-Allow-Headers", headers)
+		log.Printf(r.Method)
 
 		next.ServeHTTP(w, r)
 	})
