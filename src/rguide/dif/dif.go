fd66b5ab9475730c6f5e7d1199272e3bfe5fd6d2
Alexander <alexbidenko1998@gmail.com>
2021-03-27 20:13:44 +0300

    Fix opened ports

--- src/rguide/dif/dif.go
+++ src/rguide/dif/dif.go
@@ -5,5 +5,5 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var connStr = "host=postgres user=postgres password=postgres dbname=postgres sslmode=disable"
+var connStr = "host=188.225.47.219 user=postgres password=postgres dbname=postgres sslmode=disable"
 var DB, DBError = sqlx.Connect("postgres", connStr)
