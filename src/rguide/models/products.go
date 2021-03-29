e7b714bee421a9cb9cf44dd49da890588edba813
Alexander <alexbidenko1998@gmail.com>
2021-03-29 20:55:56 +0300

    Fix product model

--- src/rguide/models/products.go
+++ src/rguide/models/products.go
@@ -4,6 +4,7 @@ type Product struct {
 	Id int `json:"id"`
 	Title string `json:"title" valid:"required,type(string),length(1|255)"`
 	Description string `json:"description" valid:"required,type(string)"`
+	Specification string `json:"specification" valid:"type(string)"`
 	Preview string `json:"preview"`
 	Model string `json:"model"`
 }
